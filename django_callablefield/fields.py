a643a32208dd1b70a842f72cba610fd50b38e30b
Ryan Hiebert <ryan@ryanhiebert.com>
2019-09-25 16:20:41 -0500

    Without the global config

--- django_callablefield/fields.py
+++ django_callablefield/fields.py
@@ -19,30 +19,21 @@ A = Fake('A')
 B = Fake('B')
 
 
-ABFIELD_CONFIG = [
-    (A, 'a', _('A')),
-    (B, 'b', _('B')),
-]
-
-
-class ABField(models.Field):
+class ABField(models.CharField):
     empty_strings_allowed = False
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 1
         super().__init__(*args, **kwargs)
-        self.choices = [(key, name) for _, key, name in ABFIELD_CONFIG]
-        self.refs = {val: key for val, key, _ in ABFIELD_CONFIG}
-        self.derefs = {key: val for val, key, _ in ABFIELD_CONFIG}
+        self.choices = [('a', _('A')), ('b', _('B'))]
+        self.refs = {A: 'a', B: 'b'}
+        self.derefs = {'a': A, 'b': B}
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         del kwargs["max_length"]
         del kwargs["choices"]
         return name, path, args, kwargs
-
-    def get_internal_type(self):
-        return 'CharField'
 
     def from_db_value(self, value, expression, connection):
         return self.to_python(value)
@@ -59,9 +50,9 @@ class ABField(models.Field):
             return self.refs[value]
         raise Exception(f'Invalid value: {value!r}')
 
-    # def value_to_string(self, obj):
-    #     value = self.value_from_object(obj)
-    #     return self.get_prep_value(value)
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        return self.get_prep_value(value)
 
     def clean(self, value, model_instance):
         # Use the str value rather than the class for validation.
